17546de00a965c3f36cfabc16e558b55382d3751
cassie <cleder@cub.uca.edu>
2025-11-04 16:49:16 -0600

    Update CosineSimilarity

--- gutenberg_dataset/find_similar_docs/cosineSimilarity.h
+++ gutenberg_dataset/find_similar_docs/cosineSimilarity.h
@@ -1,13 +1,15 @@
 #pragma once
 #include <vector>
-#include <string>
 #include <cmath>
 #include <stdexcept>
 #include <algorithm>
+#include <limits>
 #include <map>
+#include <assert.h>
+#include "coords.h"
 
 std::vector<short> get_column(const std::vector<std::vector<short>> &v, const int &col_idx);
 float cosine_similarity(const std::vector<short> &A, const std::vector<short> &B);
-std::vector<std::vector<float>> cosine_sim(const std::vector<std::vector<short>> &A);
-std::pair<std::string, double> find_max(std::map<std::string, double> &similarities);
-std::vector<std::vector<float>>::const_iterator find_greatest(const std::vector<std::vector<float>> &similarities);
+std::vector<std::vector<float>> cosine_similarity(const std::vector<std::vector<short>> &corpus);
+bool comparision (float a, float b);
+std::pair<Coords, float> find_max(const std::vector<std::vector<float>> &similarities);
--- gutenberg_dataset/find_similar_docs/find_similar_docs.cpp
+++ gutenberg_dataset/find_similar_docs/find_similar_docs.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+#include <filesystem>
 #include <fstream>
 #include <regex>
 #include "cosineSimilarity.h"
@@ -6,9 +8,25 @@
 #include "stat.h"
 
 /*
-    Input: 1 corpus in MM format that has the same amount of vocabulary terms (dot-product limitation)
-    Output: The most similar documents in each corpus and most similar documents between corpora
+    Input: 1 corpus in MM format that has the same amount of vocabulary terms (dot-product limitation) 
+    Output: The most similar documents in each corpus and most similar documents between corpora, save them to file
 */
+const static std::filesystem::path SAVE_FOLDER = std::filesystem::current_path() / "../most_downloaded_dataset" / "docs_similarity_pointcloud";
+const static std::string PATH_DELIMITER = "\\";
+
+const int WIDTH = 10;
+template<typename T> void print(T t) {
+    std::cout << std::left << std::setw(WIDTH) << t;
+}
+
+// get the file name without the path by removing delimiter until final substring is found
+auto getPathlessFname(std::string corpus_path) {
+    while (corpus_path.find(PATH_DELIMITER) != std::string::npos) {
+        corpus_path.erase(0, corpus_path.find(PATH_DELIMITER) + PATH_DELIMITER.length());
+    }
+
+    return(corpus_path);
+}
 
 int main(const int argc, char** argv) {
     if (argc < 2) {
@@ -19,42 +37,35 @@ int main(const int argc, char** argv) {
         throw std::invalid_argument("Usage: ./find_similar_docs.exe [path to corpus]");
     }    
 
-    std::string corpus_path = std::string(argv[1]);
-
-
-    // create variables for data and calculation
-    mmReader Reader;
-
     // get 2d vector representation of Market Matrix (BoW corpus from Gensim)
-    Reader.setFilePath(corpus_path);
+    std::string corpus_path = std::string(argv[1]);
+    mmReader Reader(corpus_path);
     auto corpus = Reader.readMM();
 
     // compute upper-triangle of cosine similarity between documents in corpus
-    auto similarities = cosine_sim(corpus);
+    auto similarities = cosine_similarity(corpus);
 
     // print out cosine similarities and write to file
 
-    // get the file name without the path
-    std::regex path_delimiter("\\");
-    std::sregex_token_iterator it(corpus_path.begin(), corpus_path.end(), 
-    path_delimiter,
-    -1);
-    std::string pathless_fname = "";
-
-    std::sregex_token_iterator end;
-    for(; it != end; ++it) {
-        pathless_fname = *it;
-    }
+    // set up write to file...    
+    auto pathless_fname = getPathlessFname(corpus_path);
 
                                                             // replace _corpus.corpus with _pointcloud.csv
     std::string file_name = std::regex_replace(pathless_fname, std::regex("_corpus.corpus"), "_pointcloud.csv");
-    std::ofstream file(file_name);
+    if (!std::filesystem::exists(SAVE_FOLDER)) { // create directory
+        std::filesystem::create_directory(SAVE_FOLDER);
+    }
+    
+    // create file
+    const std::filesystem::path SAVE_PATH = SAVE_FOLDER / file_name;
+    std::ofstream file(SAVE_PATH);
 
+    // print + write
     for (int i = 0; i < similarities.size(); ++i) {
         for (int j = 0; j < similarities[i].size(); ++j) {
             // print
             if (similarities[i][j] != 0) {
-                std::cout << similarities[i][j] << " ";
+                print(similarities[i][j]);
             }
             
             // write to file
@@ -65,22 +76,18 @@ int main(const int argc, char** argv) {
                 file << similarities[i][j] << "\n";
             }
         }
-        std::cout << std::endl;
+        print("\n");
     }
+    file.close();
 
-    auto greatest = find_greatest(similarities);
-    //auto similarities = cosine_similarity_corpus(corpus);
-    // find most similar doc pair from corpus
-    //auto best_docs = find_max(similarities);
+    // find max cosine similarity between documents in corpus
+    auto max = find_max(similarities);
 
-    // output similarities / best doc pairs
-    /*
-    std::cout << "Similarities are: \n";
-    for (auto& similarity : similarities) {
-        std::cout << similarity.first << " " << similarity.second << std::endl;
-    }
-    std::cout << "Corpus' best docs are: " << best_docs.first << " " << best_docs.second << std::endl;
-    */
+    std::cout << "The greatest cosine similarity is between document " << max.first.x << " and document " << max.first.y 
+    << ". The cosine similarity is " << max.second << std::endl;
+    
+    // create van-ripps diagrams
+
 
     return(0);
 }
